986b7d017a6a4cf929fe8399dcfa96b16cd26784
yangren <ryang420@gmail.com>
2022-09-06 15:45:43 +0800

    recursion

--- src/main/java/s2recursion/Recursion.java
+++ src/main/java/s2recursion/Recursion.java
@@ -3,6 +3,35 @@ package s2recursion;
 import java.util.Arrays;
 
 public class Recursion {
+    public int power(int base, int exponent) {
+        if (exponent == 0) return 1;
+        else {
+            return base * power(base, exponent - 1);
+        }
+    }
+
+    public int productOfArray(int A[], int N) {
+        if (N == 0) return 1;
+        else {
+            return A[N - 1] * productOfArray(A, N - 1);
+        }
+    }
+
+    public String reverse(String str) {
+        if (str.isEmpty()) return "";
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty() || s.length() == 1) return true;
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        if (s.charAt(start) != s.charAt(end)) return false;
+        else
+            return isPalindrome(s.substring(start + 1, end));
+    }
     private int factorial(int n) {
         if (n < 0) {
             return -1;
